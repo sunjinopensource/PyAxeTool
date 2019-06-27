a58d95632071ba8510c8fba99caea4c892973e8a
sunjin <sjfyc@sina.com>
2019-06-27 11:57:37 +0800

    ShowMD5 -> MD5

--- PyAxeTool/__main__.py
+++ PyAxeTool/__main__.py
@@ -2,12 +2,12 @@
 # Parse command lines
 #================================================
 import argparse
-from . import ShowMD5
+from . import MD5
 
 def parseArgs():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='SubCommand')
-    ShowMD5.initArgs(subparsers)
+    MD5.initArgs(subparsers)
 
     args = parser.parse_args()
     if args.SubCommand is None:  # 目前的argparser不能识别子命令缺失的情况，因此需要手工判定
@@ -17,7 +17,7 @@ def parseArgs():
 g_Args = parseArgs()
 
 def main():
-    if ShowMD5.main(g_Args):
+    if MD5.main(g_Args):
         return
 
 main()
