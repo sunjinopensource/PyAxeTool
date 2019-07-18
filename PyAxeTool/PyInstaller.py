8730541964529b4c5e40246c1c7d276db0aea5b7
sunjin <sjfyc@sina.com>
2019-07-19 00:19:10 +0800

    去掉SkipTargets

--- PyAxeTool/PyInstaller.py
+++ PyAxeTool/PyInstaller.py
@@ -1,44 +1,45 @@
-import os
-from PyAxe import AOS, APyInstaller, ACompress, AUtil
+import os, time
+from PyAxe import ALog, AOS, APyInstaller, ACompress, AUtil
 
 def initArgs(subparsers):
     subparser = subparsers.add_parser('PyInstaller', help='制作可执行文件')
     subparser.add_argument('Targets', nargs='+', help='目标列表')
-    subparser.add_argument('--SkipTargetsOnWindows', nargs='*', help='在Windows平台上跳过的目标列表')
-    subparser.add_argument('--SkipTargetsOnLinux', nargs='*', help='在Linux平台上跳过的目标列表')
     subparser.add_argument('--OneFile', action='store_true', help='单文件形式（默认为文件夹形式）')
-    subparser.add_argument('--Pack', choices=['zip', 'tar'], help='打包方式（默认不打包）')
+    subparser.add_argument('--Pack', choices=['none', 'zip', 'tar'], help='打包方式（默认为tar）')
+    subparser.add_argument('--NonInteractive', action='store_true', help='非交互模式（默认为交互模式）')
 
 
 def getTargetsFromInput(args):
     targetMap = [['1', '所有', 'All']]
     index = 2
-    for target in enumerate(args.Targets):
-        if os.name == 'nt':
-            if target in args.SkipTargetsOnWindows:
-                continue
-        else:
-            if target in args.SkipTargetsOnLinux:
-                continue
-        targetMap.append([
-            '%d' % index,
-            target,
-            target
-        ])
+    for target in args.Targets:
+        targetMap.append([('%d' % index), target, target])
         index += 1
 
-    action = AUtil.inputChoice(targetMap, '*** 编译目标 ***')[2]
+    if args.NonInteractive:
+        action = 'All'
+    else:
+        action = AUtil.inputChoice(targetMap, '*** 编译目标 ***')[2]
+
     if action == 'All':
         return [item[2] for item in targetMap if item[2] != 'All']
     else:
         return [action]
 
 
-def makePackageFromDist(target, packStyle):
+def makePackageFromDist(target, args):
     AOS.makeDir(os.path.join('Installer', 'dist'))
     AOS.removeDir(os.path.join('Installer', 'dist', target))
+
+    # 如果存在杀软，dist/target/*文件可能被杀软占用，进而move失败
     AOS.move(os.path.join('dist', target), os.path.join('Installer', 'dist', target))
-    if packStyle:
+
+    if args.Pack is None:
+        packStyle = 'tar'
+    else:
+        packStyle = args.Pack
+
+    if packStyle != 'none':
         with AOS.ChangeDir(os.path.join('Installer', 'dist')):
             if packStyle == 'zip':
                 ACompress.zip(target, target+'.zip')
@@ -54,13 +55,15 @@ def makeTarget(target, args):
         AOS.removeDir('dist')
         extraOptions = APyInstaller.ExtraOptions()
         APyInstaller.execCommand(target+'.py', target, oneFile=args.OneFile, extraOptions=extraOptions)
-        makePackageFromDist(target, args.Pack)
+        makePackageFromDist(target, args)
         AOS.removeDir('build')
         AOS.removeDir('dist')
 
 
 def main(args):
     if args.SubCommand == 'PyInstaller':
+        # 执行前，请关闭杀软，否则可能失败
+        ALog.enableFileSink(False)
         finalTargets = getTargetsFromInput(args)
         for target in finalTargets:
             makeTarget(target, args)
